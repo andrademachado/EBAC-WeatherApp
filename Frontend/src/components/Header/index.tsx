8e24022d99db82c027a34589994b3f68bf484c21
Lígia Andrade <andradeligiamp@gmail.com>
2024-05-31 17:22:54 -0300

    construção do header

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -2,20 +2,26 @@ import * as S from './styles'
 
 const Header = () => {
     return (
-            <header>
-                <div className="Container">
-                <div className="header-dark">
-                    <input className="form-control" />
-                    <button >pesquisar</button>
-                </div>        
-            </div>
-            </header>
+            <S.Header>
+                    <div className='logo' >
+                    <img src="https://via.placeholder.com/40x40" />
+                    <S.Title>
+                        EBACLIMA                   
+                    </S.Title> 
+
+                    </div>
+               
+                    <div className='formButton'>
+                        <input className='form' type='text' placeholder='Digite  sua cidade' />
+                        <button className='button' type='submit'>pesquisar</button>
+                    </div>
+                    <div className='buttonLocalizacao'>
+                        <button type='submit' className='localizacao'>Minha Localização</button>
+                    </div>
+                
+
+            </S.Header>
     )
 }
-
-
-
-
-
 
 export default Header
--- Frontend/src/components/Header/styles.ts
+++ Frontend/src/components/Header/styles.ts
@@ -1,17 +1,77 @@
 import styled from 'styled-components';
-import { colors } from "../../styles/colors";
+
+export const Header = styled.header`
+    max-width: 1440px;
+    width: 100%;
 
 
+    /* Ver a possibilidade de aumentar o height: 80px; e demais espaçamentos */
 
-
-export const Header = styled.div`
-
-    margin-left:8px;
-    height:72px;
-    width : 100%
-    color :red ;
-    background-color: ${colors.cabecalho};
+    height: 68px;
+    margin-left: 0;
+    background-color: #ECF4F6;
     align-items: center;
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    gap: 42px;
+    
+    .logo{
+        display: flex;
+        width: 136px;
+        height: 48px;
+        margin-top: 10px;
+        margin-left: 0;
+        margin-bottom: 16px;
+
+        
+    }
     
 
-`;
+    .formButton {    
+        border-radius: 16px;
+      
+        
+        
+    }
+
+    
+    .form{
+        background-color: yellow;
+        height: 30px;
+        width: 250px;
+        text-align: center;
+        border-radius: 16px;          
+    }
+
+    .button{
+        background-color: red;
+        height: 30px;
+        width: 65px;
+        text-align: center;
+        border-radius: 16px;
+        cursor: pointer;
+    }
+
+    
+
+    .localizacao{
+        background-color: red;
+        height: 35px;
+        width:100px;
+        justify-content: end;
+        border-radius: 16px;
+        padding-right: 0  1em 0 auto; 
+        cursor: pointer; 
+    
+    }
+`
+
+export const Title = styled.h3`
+    color:red;
+    text-align: left;
+`
+
+
+
+
