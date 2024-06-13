69a86e12bc1dbfca992cd6583eeac1ba5a49e0cc
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-06-13 11:53:01 -0300

    pequenos ajustes

--- Frontend/src/components/Astro/index.tsx
+++ Frontend/src/components/Astro/index.tsx
@@ -1,11 +1,21 @@
+import { useSelector } from 'react-redux'
+
+import { RootReducer } from '../../store'
+import { useGetNewLocationQuery } from '../../services/api'
+
 import * as S from './styles'
 
 import sun from '../../assets/astro-sun.png'
 import moon from '../../assets/astro-moon.png'
 
 const Astro = () => {
+
+    const storedLocation = useSelector((state: RootReducer) => state.mainPlace.place)
+
+    const { data } = useGetNewLocationQuery(storedLocation)
+
     return (
-        <S.AstroContainer>
+        <S.AstroContainer className='fadeIn'>
             <S.SunContainer>
                 <S.AstroTitle>
                     <img src={sun} alt="Ícone Sol" />
@@ -19,15 +29,17 @@ const Astro = () => {
                                 style={{ '--p': '50', '--c': '#C7C8CC', '--b': '1px', '--w': '212px' } as React.CSSProperties}
                             ></div>
                         </div>
-                        <div className="astroLine">
-                            <div
-                                className="pie animate"
-                                style={{ '--p': '35', '--c': '#F6C833', '--b': '3px', '--w': '212px' } as React.CSSProperties}
-                            ></div>
-                        </div>
+                        {!data?.forecast.forecastday[0].astro.is_sun_up && (
+                            <div className="astroLine">
+                                <div
+                                    className="pie animate"
+                                    style={{ '--p': '35', '--c': '#F6C833', '--b': '3px', '--w': '212px' } as React.CSSProperties}
+                                ></div>
+                            </div>
+                        )}
                     </div>
                 </S.AstroDataContainer>
-                <S.AstroApiData style={{padding: `0 50px 0 30px`}}>
+                <S.AstroApiData style={{ padding: `0 50px 0 30px` }}>
                     <div className='astroHour'>
                         <span className='astroTxt'>Nascer do Sol</span>
                         <span className='astroTime'>07:06</span>
@@ -52,15 +64,17 @@ const Astro = () => {
                                     style={{ '--p': '50', '--c': '#C7C8CC', '--b': '1px', '--w': '212px' } as React.CSSProperties}
                                 ></div>
                             </div>
-                            <div className="astroLine">
-                                <div
-                                    className="pie animate"
-                                    style={{ '--p': '35', '--c': '#818FB4', '--b': '3px', '--w': '212px' } as React.CSSProperties}
-                                ></div>
-                            </div>
+                            {data?.forecast.forecastday[0].astro.is_moon_up && (
+                                <div className="astroLine">
+                                    <div
+                                        className="pie animate"
+                                        style={{ '--p': '20', '--c': '#818FB4', '--b': '3px', '--w': '212px' } as React.CSSProperties}
+                                    ></div>
+                                </div>
+                            )}
                         </div>
                     </S.AstroDataContainer>
-                    <S.AstroApiData style={{padding: `0 36px 0 68px`}}>
+                    <S.AstroApiData style={{ padding: `0 36px 0 68px` }}>
                         <div className='astroHour'>
                             <span className='astroTxt'>Luar</span>
                             <span className='astroTime'>07:06</span>
