75dab29661b54a41279e42c0397fa6797eb4cb16
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-06-13 23:49:04 -0300

    logica do sol v1 finalizada

--- Frontend/src/components/Astro/index.tsx
+++ Frontend/src/components/Astro/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { RootReducer } from '../../store'
@@ -15,22 +15,22 @@ const Astro = () => {
 
     const { data } = useGetNewLocationQuery(storedLocation)
 
-    const [sunValue, setSunValue] = useState<string>("45")
+    const [sunValue, setSunValue] = useState<string>("")
     const [moonValue, setMoonValue] = useState<string>("13")
 
     const timeConvertion = [
-        {am: "01", pm: "13"},
-        {am: "02", pm: "14"},
-        {am: "03", pm: "15"},
-        {am: "04", pm: "16"},
-        {am: "05", pm: "17"},
-        {am: "06", pm: "18"},
-        {am: "07", pm: "19"},
-        {am: "08", pm: "20"},
-        {am: "09", pm: "21"},
-        {am: "10", pm: "22"},
-        {am: "11", pm: "23"},
-        {am: "12", pm: "12"}
+        { am: "01", pm: "13" },
+        { am: "02", pm: "14" },
+        { am: "03", pm: "15" },
+        { am: "04", pm: "16" },
+        { am: "05", pm: "17" },
+        { am: "06", pm: "18" },
+        { am: "07", pm: "19" },
+        { am: "08", pm: "20" },
+        { am: "09", pm: "21" },
+        { am: "10", pm: "22" },
+        { am: "11", pm: "23" },
+        { am: "12", pm: "12" }
     ]
 
     const handleTime = (time: string | undefined) => {
@@ -51,6 +51,42 @@ const Astro = () => {
         }
     }
 
+    const timeToMinutes = (time: string) => {
+        const [hours, minutes] = time.split(':').map(Number)
+        return hours * 60 + minutes
+    }
+
+    useEffect(() => {
+
+        if (!data) return
+
+        const sunRise = String(handleTime(data.forecast.forecastday[0].astro.sunrise))
+        const sunSet = String(handleTime(data.forecast.forecastday[0].astro.sunset))
+        const currentTime = data?.current.last_updated.slice(11, 16)
+
+        // Convert timeStart, timeEnd, and currentTime to minutes
+        const sunStartMinutes = timeToMinutes(sunRise)
+        const sunEndMinutes = timeToMinutes(sunSet)
+        const currentMinutes = timeToMinutes(currentTime)
+
+        // Calculate the total duration and the elapsed duration
+        const sunTotalDuration = sunEndMinutes - sunStartMinutes
+        const sunElapsedDuration = currentMinutes - sunStartMinutes
+
+        // Calculate the percentage of the period that has passed
+        const sunPercentagePassed = (sunElapsedDuration / sunTotalDuration) * 100 / 2
+
+        if (sunPercentagePassed < 0 || sunPercentagePassed > 50) {
+            setSunValue("0")
+            console.log("out of range" + sunValue)
+        } else {
+            setSunValue(`${Math.floor(sunPercentagePassed)}`)
+        }
+
+        /* console.log(`Percentage of the period that has passed: ${sunPercentagePassed.toFixed(2)}%`) */
+
+    }, [data])
+
     return (
         <S.AstroContainer className='fadeIn'>
             <S.SunContainer>
@@ -66,19 +102,17 @@ const Astro = () => {
                                 style={{ '--p': '50', '--c': '#C7C8CC', '--b': '1px', '--w': '212px' } as React.CSSProperties}
                             ></div>
                         </div>
-                        {!data?.forecast.forecastday[0].astro.is_sun_up && (
-                            <div className="astroLine">
-                                <div
-                                    className="pie animate"
-                                    style={{ '--p': `${sunValue}`, '--c': '#F6C833', '--b': '3px', '--w': '212px' } as React.CSSProperties}
-                                ></div>
-                            </div>
-                        )}
+                        <div className="astroLine">
+                            <div
+                                className="pie animate"
+                                style={{ '--p': `${sunValue}`, '--c': '#F6C833', '--b': '3px', '--w': '212px' } as React.CSSProperties}
+                            ></div>
+                        </div>
                     </div>
                 </S.AstroDataContainer>
                 <S.AstroApiData style={{ padding: `0 50px 0 30px` }}>
                     <div className='astroHour'>
-                        <span className='astroTxt'>Nascer do Sol</span>
+                        <span className='astroTxt'>Nascer do Sol {sunValue}</span>
                         <span className='astroTime'>{handleTime(data?.forecast.forecastday[0].astro.sunrise)}</span>
                     </div>
                     <div className='astroHour'>
@@ -101,14 +135,12 @@ const Astro = () => {
                                     style={{ '--p': '50', '--c': '#C7C8CC', '--b': '1px', '--w': '212px' } as React.CSSProperties}
                                 ></div>
                             </div>
-                            {data?.forecast.forecastday[0].astro.is_moon_up && (
-                                <div className="astroLine">
-                                    <div
-                                        className="pie animate"
-                                        style={{ '--p': `${moonValue}`, '--c': '#818FB4', '--b': '3px', '--w': '212px' } as React.CSSProperties}
-                                    ></div>
-                                </div>
-                            )}
+                            <div className="astroLine">
+                                <div
+                                    className="pie animate"
+                                    style={{ '--p': `${moonValue}`, '--c': '#818FB4', '--b': '3px', '--w': '212px' } as React.CSSProperties}
+                                ></div>
+                            </div>
                         </div>
                     </S.AstroDataContainer>
                     <S.AstroApiData style={{ padding: `0 36px 0 58px` }}>
